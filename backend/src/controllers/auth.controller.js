84ad5656456793d586ae3f5aa11add9d2e91b490
Aman yadav <amanyadav@Amans-MacBook-Air.local>
2026-08-09 15:42:02 +0530

    Fix production authentication cookies

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -117,8 +117,8 @@ export const signinUser = async (req, res, next) => {
           .status(200)
           .cookie("access_token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           })
           .json({
